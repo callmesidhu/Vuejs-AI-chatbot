dfc190fe7ab6c58f438b139482afe66cd0d55032
callmesidhu <sidharthpunalur@gmail.com>
2024-07-09 03:36:39 +0530

    create build

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div class="flex flex-row h-screen p-6">
     <div class="lg:flex w-full">
       <div class="lg:w-[15%] lg:mr-6 mr-0">
-        <img class="aspect-auto w-full items-center mb-5" src="../AIBot.png" />
+        <img class="aspect-auto w-full items-center mb-5" src="../public/AIBot.png" alt="AI Bot"/>
         <h1 class="text-center font-bold text-2xl mb-8">Our AI Build with Gemini API</h1>
       </div>
       <div class="lg:w-[85%] h-full">
